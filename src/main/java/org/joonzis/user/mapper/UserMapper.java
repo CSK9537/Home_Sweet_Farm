83f997a93a52b05207afff71cccd5f9e52797408
hyelinwi57-cell <hyelinwi57@gmail.com>
2026-02-03 11:01:56 +0900

    UserVO 수정 및 UserMapper 수정, 테스트
    src/test/java/org/joonzis/user/service/UserServiceTests.jav#

--- src/main/java/org/joonzis/user/mapper/UserMapper.java
+++ src/main/java/org/joonzis/user/mapper/UserMapper.java
@@ -8,24 +8,29 @@ public interface UserMapper {
 	//데이터 넣기
 	public void insert(UserVO vo);
 	//데이터 조회
-	public  UserVO selectLogin(int USER_ID);
+	public  UserVO selectLogin(int user_id);
 	//데이터 삭제
 	public int delete(UserVO vo);
 	
 	//아이디 찾기(이메일)
-	public String findIdByEmail(@Param("EMAIL")String EMAIL);
+	public String findIdByEmail(@Param("email")String email);
 	
 	//아이디 찾기(전화번호)
-	public String findIdByPhone(@Param("PHONE")String PHONE);
+	public String findIdByPhone(@Param("phone")String phone);
 	
 	//비밀번호 찾기 대상 확인(이메일)
-	public int existUserByEmail(@Param("USERNAME")String USERNAME,
-						@Param("EMAIL")String EMAIL);
+	public int existUserByEmail(@Param("username")String username,
+						@Param("email")String email);
 	
 	//비밀번호 찾기 대상 확인(전화번호)
-	public int existUserByPhone(@Param("USERNAME")String USERNAME,
-			@Param("PHONE")String PHONE);
+	public int existUserByPhone(@Param("username")String username,
+			@Param("phone")String phone);
 	
 	//비밀번호 재설정
 	public void updatePw(UserVO vo);
+
+	//아이디 중복 체크
+	public int countByUsername(@Param("username")String username);
+	
+	
 }
