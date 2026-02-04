d57720d20dcca025ff82b13e87e6e2011585c29a
CSK9537 <rct9537@gmail.com>
2026-02-05 00:59:00 +0900

    [feat] crawl, plant service 추가

---
+++ src/main/java/org/joonzis/plant/service/CrawlServiceImpl.java
@@ -0,0 +1,8 @@
+package org.joonzis.plant.service;
+
+public class CrawlServiceImpl implements CrawlService{
+	// 드라이버를 따로 만들고 호출할지 고민
+	// 식물 이름 목록 DB 저장
+	// 백과사전 정보 입력(식물 이름 목록 DB 기반으로 백과사전 페이지 이동 후 DB 저장)
+	// 관리가이드 정보 입력(식물 이름 목록 DB 기반으로 관리가이드 페이지 이동 후 DB 저장)
+}
