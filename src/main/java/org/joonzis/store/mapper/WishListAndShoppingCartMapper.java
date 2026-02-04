56c265e1390fd4aae6c8c8dc2ef6668f842fba29
jinjin7576 <jinjin7576@daum.net>
2026-02-04 17:51:24 +0900

    [feat] 찜목록&장바구니 비즈니스 로직 추가 및 Mapper 수정

--- src/main/java/org/joonzis/store/mapper/WishListAndShoppingCartMapper.java
+++ src/main/java/org/joonzis/store/mapper/WishListAndShoppingCartMapper.java
@@ -2,6 +2,7 @@ package org.joonzis.store.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.joonzis.store.dto.ShoppingCartDTO;
 import org.joonzis.store.dto.WishListDTO;
 import org.joonzis.store.vo.ShoppingCartVO;
@@ -10,13 +11,20 @@ import org.joonzis.store.vo.WishListVO;
 public interface WishListAndShoppingCartMapper {
 	// 찜목록
 	public int insertWishList(WishListVO vo);
+	public List<WishListDTO> getWishList(@Param("data")int param, @Param("type")String type);
+	// 위 함수가 테스트를 통과하면 밑에 두 함수는 삭제해도 됨
 	public List<WishListDTO> getWishListByUserId(int user_id);
-	public List<WishListDTO> getWishListByProductId(int product_id); // 관리자용?
+	public List<WishListDTO> getWishListByProductId(int product_id);
 	public int deleteWishList(WishListVO vo);
 	
 	// 장바구니
 	public int insertShoppingCart(ShoppingCartVO vo);
+	public List<ShoppingCartDTO> getShoppingCart(@Param("data")int param, @Param("type")String type);
+	// 위 함수가 테스트를 통과하면 밑에 두 함수는 삭제해도 됨
 	public List<ShoppingCartDTO> getShoppingCartByUserId(int user_id);
 	public List<ShoppingCartDTO> getShoppingCartByProductId(int product_id);
-	public int deleteShopingCart(ShoppingCartVO vo);
+	public int deleteShopingCart(@Param("user_id") int user_id, @Param("product_id") int product_id);
+	
+	// Upsert 이미 있으면 update, 데이터가 없으면 insert
+	public int upsertShoppingCart(@Param("user_id") int user_id, @Param("product_id") int product_id);
 }
--- src/test/java/org/joonzis/store/mapper/WishListAndShoppingCartMapperTests.java
+++ src/test/java/org/joonzis/store/mapper/WishListAndShoppingCartMapperTests.java
@@ -114,4 +114,69 @@ public class WishListAndShoppingCartMapperTests {
 //		vo.setProduct_id(2);
 //		log.info("장바구니 삭제 테스트 실행 결과 : " + mapper.deleteShopingCart(vo) + "개의 행이 삭제 됨.");
 //	}
+
+//	@Test
+//	public void upsertCartTest() {
+//		ShoppingCartVO vo = new ShoppingCartVO();
+//		vo.setUser_id(2);
+//		vo.setProduct_id(2);
+//		log.info("upsert 결과 : " + mapper.upsertShoppingCart(vo));
+//		
+//	}
+	
+//	@Test
+//	public void getWishListTest() {
+//		int param = 2;
+//		String product = "product";
+//		String user = "user";
+//		
+//		List<WishListDTO> list = mapper.getWishList(param, product);
+//		log.info("파라미터 값 : " + param);
+//		log.info("조회 타입 : " + product);
+//		if(list.size() > 0) {
+//			for (WishListDTO wishListDTO : list) {
+//				log.info(wishListDTO);
+//			}
+//		} else {
+//			log.info("리스트의 길이가 0임");
+//		}
+//		
+//		list = mapper.getWishList(param, user);
+//		log.info("조회 타입 : " + user);
+//		if(list.size() > 0) {
+//			for (WishListDTO wishListDTO : list) {
+//				log.info(wishListDTO);
+//			}
+//		} else {
+//			log.info("리스트의 길이가 0임");
+//		}
+//	}
+	
+//	@Test
+//	public void getShoppingCartTest() {
+//		int param = 2;
+//		String product = "product";
+//		String user = "user";
+//		
+//		List<ShoppingCartDTO> list = mapper.getShoppingCart(param, product);
+//		log.info("파라미터 값 : " + param);
+//		log.info("조회 타입 : " + product);
+//		if(list.size() > 0) {
+//			for (ShoppingCartDTO dto : list) {
+//				log.info(dto);
+//			}
+//		} else {
+//			log.info("리스트의 길이가 0임");
+//		}
+//		
+//		list = mapper.getShoppingCart(param, user);
+//		log.info("조회 타입 : " + user);
+//		if(list.size() > 0) {
+//			for (ShoppingCartDTO dto : list) {
+//				log.info(dto);
+//			}
+//		} else {
+//			log.info("리스트의 길이가 0임");
+//		}
+//	}
 }
