a49724721203bef4bc9a63605be1f00582362f38
hyelinwi57-cell <hyelinwi57@gmail.com>
2026-02-04 15:45:42 +0900

    [feat]마케팅정보수신 동의2

--- src/main/java/org/joonzis/user/service/UserServiceImpl.java
+++ src/main/java/org/joonzis/user/service/UserServiceImpl.java
@@ -21,18 +21,13 @@ public class UserServiceImpl implements UserService{
 				!vo.getPassword().equals(vo.getConfirmPassword())) {
 			throw new IllegalArgumentException("비밀번호 확인이 일치하지 않습니다.");
 		}
-		
-		
 		//2)아이디 중복 체크(예시)
 		if(isIdDuplicate(vo.getUsername())) {
 			throw new IllegalStateException("이미 존재하는 아이디입니다");
 		}
-		
 		//3)마케팅 정보 수신 동의 처리
 		//confirm_event: 0(미동의), 1(동의)
 		vo.setConfirm_event(vo.getConfirm_event() == 1? 1 : 0);
-		
-		
 		//4)DB insert
 		int result = usermapper.insert(vo);
 		if(result != 1) {
