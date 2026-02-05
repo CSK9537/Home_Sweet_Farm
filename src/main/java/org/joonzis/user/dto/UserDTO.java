471c2581053538090e61e1de2123297d51e46fe8
WiHyelin <hyelinwi57@gmail.com>
2026-02-05 15:14:44 +0900

    [feat]공개형 프로필 조회 수정

--- src/main/java/org/joonzis/user/dto/UserDTO.java
+++ src/main/java/org/joonzis/user/dto/UserDTO.java
@@ -18,7 +18,7 @@ public class UserDTO {
 	//2)소개
 	private String intro;
 	//3)활동내역, 주요활동분야(추후 추가 예정)
-	//4)통계
-	private int reply_cnt, view_cnt;//채택 답변도 추가 예정
+	//4)통계(전체답변, 조회수, 채택 답변수)
+	private int reply_cnt, view_cnt, is_selected;
 	//5)Q&A등급은 미정
 }
