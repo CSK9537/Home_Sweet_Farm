5d3602c4157908efe8d9bc6fa457fe7cbc5b9e82
csh1217 <jim1217916@naver.com>
2026-02-06 10:57:51 +0900

    [feature]채팅 - 파일 이동

--- src/main/java/org/joonzis/chatting/service/ChatService.java
+++ src/main/java/org/joonzis/chatting/service/ChatService.java
@@ -2,13 +2,14 @@ package org.joonzis.chatting.service;
 
 import java.util.List;
 
+import org.joonzis.chatting.dto.ChatRoomDTO;
 import org.joonzis.chatting.vo.MsgVO;
 import org.joonzis.chatting.vo.RoomVO;
 
 public interface ChatService {
-	public void sendMessage(int sender_id, int receiver_id, MsgVO msgVO);
+	public MsgVO sendMessage(int sender_id, int receiver_id, MsgVO msgVO);
 	public List<MsgVO> getMessages(int user_id, int room_id);
-	public List<RoomVO> getUserRooms(int user_id);
+	public List<ChatRoomDTO> getUserRooms(int user_id);
 	public void readMessage(int user_id, int room_id);
 	public int createRoom(int user1_id, int user2_id);
 }
