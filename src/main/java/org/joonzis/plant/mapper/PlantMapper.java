10712df4e76fe0fbd4009fad3041f8ff16ec6ea9
csk9537 <rct9537@gmail.com>
2026-02-06 17:27:19 +0900

    [feat] 가이드 DB 작업

--- src/main/java/org/joonzis/plant/mapper/PlantMapper.java
+++ src/main/java/org/joonzis/plant/mapper/PlantMapper.java
@@ -1,8 +1,14 @@
 package org.joonzis.plant.mapper;
 
+import java.util.List;
+
 import org.joonzis.plant.vo.PlantVO;
 
 public interface PlantMapper {
 	// 식물 정보 입력
 	public int insertPlantInfo(PlantVO pvo);
+	// 식물 번호 목록 가져오기
+	public List<Integer> plantIdList();
+	// 식물 정보 출력(plant_id)
+	public PlantVO getPlantInfo(int plant_id);
 }
--- src/main/java/org/joonzis/plant/service/CrawlService.java
+++ src/main/java/org/joonzis/plant/service/CrawlService.java
@@ -8,6 +8,6 @@ public interface CrawlService {
 	public void insertPlantNames(List<String> list);
 	// 백과사전 전체 정보 입력(식물 이름 목록 DB 기반으로 PictureThis 백과사전 페이지 이동 후 DB 저장)
 	public void insertTotalPlantData(List<String> list);
-	// 관리가이드 전체 정보 입력(식물 이름 목록 DB 기반으로 PictureThis 관리가이드 페이지 이동 후 DB 저장)
-	public void insertTotalGuideData(List<String> list);
+	// 관리가이드 전체 정보 입력(백과사전 DB 기반으로 PictureThis 관리가이드 페이지 이동 후 DB 저장)
+	public void insertTotalGuideData(List<Integer> list);
 }
