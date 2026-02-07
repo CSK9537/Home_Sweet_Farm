06a7e7743abf56b57f975222eab123b1a6080473
jinjin7576 <jinjin7576@daum.net>
2026-02-07 14:49:00 +0900

    Update StoreController.java

--- src/main/java/org/joonzis/store/controller/StoreController.java
+++ src/main/java/org/joonzis/store/controller/StoreController.java
@@ -2,20 +2,13 @@ package org.joonzis.store.controller;
 
 import java.util.List;
 
-import org.joonzis.store.dto.ProductReviewDTO;
-import org.joonzis.store.mapper.WishListAndShoppingCartMapper;
 import org.joonzis.store.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j;
 
