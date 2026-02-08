215afdfb91b1156d452cd2fe77fb738c9a9e8c6a
jinjin7576 <jinjin7576@daum.net>
2026-02-08 18:25:02 +0900

    [feat] 장바구니&찜목록 컨트롤러
    작업 요약 :
    제품 리뷰 기능 구현, 쿼리 약간 수정
    장바구니&찜목록 기능 구현 중
    
    특이사항 :
    서브 노트북으로 작업하다가 브랜치 꼬여서 새로운 브랜치 생성

--- src/main/java/org/joonzis/store/controller/OrderController.java
+++ src/main/java/org/joonzis/store/controller/OrderController.java
@@ -1,13 +1,67 @@
 package org.joonzis.store.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.joonzis.store.dto.OrderDTO;
+import org.joonzis.store.dto.OrderWrapper;
+import org.joonzis.store.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 @RequestMapping("store/order")
 public class OrderController {
-
+	@Autowired
+	OrderService oService;
+	
+	// 주문 내역 추가
+	@PostMapping(
+			value = "/add/{user_id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> addOrder(@RequestBody OrderWrapper oWrapper){
+		try {
+			oService.addOrder(oWrapper);
+			return new ResponseEntity<String>("success",HttpStatus.OK);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	// 주문 내역 조회
+	@GetMapping(
+			value = "/getDetail/{order_id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<OrderDTO> getOrderDetail(@PathVariable("order_id")String order_id){
+		return new ResponseEntity<OrderDTO>(oService.getOrderDetail(order_id), HttpStatus.OK);
+	}
+	
+	// 유저의 주문 내역 리스트 조회
+	@GetMapping(
+			value = "/getList/user_id/{user_id}}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<OrderDTO>> getOrderListByUserId(@PathVariable("user_id")int user_id){
+		List<OrderDTO> list = oService.getOrderListByUserId(user_id);
+		return new ResponseEntity<List<OrderDTO>>(list,HttpStatus.OK);
+	}
+	
+	// 상품 기준 주문 내역 리스트 조회
+	@GetMapping(
+			value = "/getList/product_id/{product_id}}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<OrderDTO>> getOrderListByProductId(@PathVariable("product_id")int product_id){
+		List<OrderDTO> list = oService.getOrderListByProductId(product_id);
+		return new ResponseEntity<List<OrderDTO>>(list,HttpStatus.OK);
+	}
 }
--- src/main/java/org/joonzis/store/controller/ReviewController.java
+++ src/main/java/org/joonzis/store/controller/ReviewController.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.joonzis.store.dto.ProductReviewDTO;
 import org.joonzis.store.service.StoreService;
+import org.joonzis.store.vo.ProductReviewVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +26,58 @@ public class ReviewController {
 	@Autowired
 	StoreService sService;
 	
-	// 댓글 전체 보기(REST)
+	// 제품 기준 리뷰 전체 보기(REST)
 	@GetMapping(
 			value="/get/product/{product_id}",
 			produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ProductReviewDTO>> ReviewList(@PathVariable int product_id){
 		List<ProductReviewDTO> list = sService.getReviewListByProductId(product_id);
+		return new ResponseEntity<List<ProductReviewDTO>>(list, HttpStatus.OK);
+	}
+	
+	// 리뷰 추가
+	@PostMapping(
+			value="add/product/{product_id}/user/{user_id}")
+	public ResponseEntity<String> addReview(
+			@PathVariable("product_id")int product_id,
+			@PathVariable("user_id")int user_id,
+			@RequestBody ProductReviewVO vo){
+		vo.setProduct_id(product_id);
+		vo.setWriter_id(user_id);
+		if (sService.addProductReview(vo) == 0) return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<String>("OK" ,HttpStatus.OK);
+	}
+	
+	// 리뷰 수정
+	@PutMapping(
+			value = "modify/review/{product_review_id}",
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> modifyProductReview(
+			@PathVariable("product_review_id") int product_review_id,
+			@RequestBody ProductReviewVO vo){
+		vo.setProduct_review_id(product_review_id);
+		if(sService.modifyProductReview(vo) == 0) return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<String>(HttpStatus.OK);
+	}
+	
+	// 리뷰 삭제
+	@DeleteMapping(
+			value = "remove/review/{product_review_id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> removeReview(
+			@PathVariable("product_review_id")int product_review_id){
+		int result = sService.removeProductReview(product_review_id);
+		if(result > 0) return new ResponseEntity<String>(HttpStatus.OK);
+		else return new ResponseEntity<String>("Database does not changed",HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	// 유저가 쓴 리뷰들 조회
+	@GetMapping(
+			value="get/user/{user_id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ProductReviewDTO>> getReviewsByUserId(
+			@PathVariable("user_id") int user_id){
+		List<ProductReviewDTO> list = sService.getReviewListByUserId(user_id);
 		return new ResponseEntity<List<ProductReviewDTO>>(list, HttpStatus.OK);
 	}
 }
