1ae9ad619bccc8da7dfcfdef4c449773fc77c8a9
jinjin7576 <jinjin7576@daum.net>
2026-02-11 11:24:48 +0900

    [fix] 이메일 받는 요청 변경

--- src/main/java/org/joonzis/user/controller/EmailAuthControler.java
+++ src/main/java/org/joonzis/user/controller/EmailAuthControler.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class EmailAuthControler {
 			value = "/send",
 			produces = "text/plain;charset=UTF-8")
 	public ResponseEntity<String> sendEmail(
-			@RequestParam("email") String email,
+			@RequestBody String email,
 			HttpSession session){
 		String code = emailer.createRandomNumber();
 		log.info("생성된 코드 : " + code);
