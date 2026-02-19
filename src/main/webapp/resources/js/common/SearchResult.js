38d0e69a4a1b1224a01b66d62d2c719e34af2d23
csk9537 <rct9537@gmail.com>
2026-02-19 17:44:41 +0900

    [feat] 검색 기능 부분적 완료

---
+++ src/main/webapp/resources/js/common/SearchResult.js
@@ -0,0 +1,13 @@
+let q = document.getElementById('q').innerHTML;
+let slist = document.querySelector('.slist');
+
+(function getList(q){
+	fetch('/search/plant?q=' + q)
+		.then(response => response.json())
+		.then(data => {
+			data.forEach(element => {
+				console.log(element);
+			});
+		})
+		.catch(err => console.log(err));
+})(q);
