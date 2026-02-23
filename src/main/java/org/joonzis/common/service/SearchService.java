631228af7647e988a48a3644e46992bf5c33fd1c
csk9537 <rct9537@gmail.com>
2026-02-23 14:07:12 +0900

    [feat] 검색 기능 프로토타이핑

--- src/main/java/org/joonzis/common/service/SearchService.java
+++ src/main/java/org/joonzis/common/service/SearchService.java
@@ -2,12 +2,25 @@ package org.joonzis.common.service;
 
 import java.util.List;
 
+import org.joonzis.community.vo.BoardVO;
 import org.joonzis.plant.dto.SimplePlantDTO;
+import org.joonzis.store.vo.ProductVO;
 
 public interface SearchService {
 	// 커뮤니티 검색 리스트
+	// 제목
+	public List<BoardVO> searchBoardListByTitle(String q);
+	// 내용
+	public List<BoardVO> searchBoardListByContent(String q);
+	// 작성자
+	public List<BoardVO> searchBoardListByWriter(String q);
+	
 	// 식물 검색 리스트
 	public List<SimplePlantDTO> searchPlantList(String q);
+	
 	// 스토어 검색 리스트
+	public List<ProductVO> searchProductList(String q);
+	
 	// Q&A 검색 리스트
+	
 }
--- src/test/java/org/joonzis/common/mapper/SearchMapperTests.java
+++ src/test/java/org/joonzis/common/mapper/SearchMapperTests.java
@@ -3,7 +3,9 @@ package org.joonzis.common.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joonzis.community.vo.BoardVO;
 import org.joonzis.plant.dto.SimplePlantDTO;
+import org.joonzis.store.vo.ProductVO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,54 @@ public class SearchMapperTests {
 	private SearchMapper smapper;
 	
 //	@Test
+//	public void searchBoardsByTitleTest() {
+//		log.info("search boards by title test");
+//		try {
+//			List<BoardVO> list = new ArrayList<BoardVO>();
+//			list = smapper.searchBoardsByTitle("제목");
+//			int i = 1;
+//			for(BoardVO vo : list) {
+//				log.info((i++) + "번째");
+//				log.info(vo.getTitle());
+//			}
+//		} catch (Exception e) {
+//			log.error(e);
+//		}
+//	}
+//	
+//	@Test
+//	public void searchBoardsByContentTest() {
+//		log.info("search boards by content test");
+//		try {
+//			List<BoardVO> list = new ArrayList<BoardVO>();
+//			list = smapper.searchBoardsByContent("내용");
+//			int i = 1;
+//			for(BoardVO vo : list) {
+//				log.info((i++) + "번째");
+//				log.info(vo.getContent());
+//			}
+//		} catch (Exception e) {
+//			log.error(e);
+//		}
+//	}
+	
+//	@Test
+//	public void searchBoardsByWriterTest() {
+//		log.info("search boards by writer test");
+//		try {
+//			List<BoardVO> list = new ArrayList<BoardVO>();
+//			list = smapper.searchBoardsByWriter("mem");
+//			int i = 1;
+//			for(BoardVO vo : list) {
+//				log.info((i++) + "번째");
+//				log.info(vo.getContent());
+//			}
+//		} catch (Exception e) {
+//			log.error(e);
+//		}
+//	}
+	
+//	@Test
 //	public void searchPlantsTest() {
 //		log.info("search plants test...");
 //		try {
@@ -32,6 +82,22 @@ public class SearchMapperTests {
 //				log.info((i++) + "번째");
 //				log.info(dto.getPlant_name());
 //				log.info(dto.getPlant_name_kor());
+//			}
+//		} catch (Exception e) {
+//			log.error(e);
+//		}
+//	}
+	
+//	@Test
+//	public void searchProductsTest() {
+//		log.info("search products test...");
+//		try {
+//			List<ProductVO> list = new ArrayList<ProductVO>();
+//			list = smapper.searchProducts("테스트");
+//			int i = 1;
+//			for(ProductVO vo : list) {
+//				log.info((i++) + "번째");
+//				log.info(vo.getProduct_name());
 //			}
 //		} catch (Exception e) {
 //			log.error(e);
