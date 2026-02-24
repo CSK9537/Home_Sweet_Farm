9f57d80657fa176f8c533377028db0797b5eb8a0
CSK9537 <rct9537@gmail.com>
2026-02-25 04:01:51 +0900

    [feat] SearchService 완료

--- src/main/webapp/resources/js/common/SearchResult.js
+++ src/main/webapp/resources/js/common/SearchResult.js
@@ -13,3 +13,49 @@ service.getBoardsByTitle(q, jsonArray => {
     tmp.innerHTML += makechild;
   });
 });
+service.getBoardsByContent(q, jsonArray => {
+  jsonArray.forEach(board => {
+    let makechild = '';
+    makechild += `<div>`;
+    makechild += `${board.title}\n`;
+    makechild += `${board.content}\n`;
+    makechild += `${board.user_id}`;
+    makechild += `</div>`;
+
+    tmp.innerHTML += makechild;
+  });
+});
+service.getBoardsByWriter(q, jsonArray => {
+  jsonArray.forEach(board => {
+    let makechild = '';
+    makechild += `<div>`;
+    makechild += `${board.title}\n`;
+    makechild += `${board.content}\n`;
+    makechild += `${board.user_id}`;
+    makechild += `</div>`;
+
+    tmp.innerHTML += makechild;
+  });
+});
+service.getPlants(q, jsonArray => {
+  jsonArray.forEach(plant => {
+    let makechild = '';
+    makechild += `<div>`;
+    makechild += `${plant.plant_name}\n`;
+    makechild += `${plant.plant_name_kor}`;
+    makechild += `</div>`;
+
+    tmp.innerHTML += makechild;
+  });
+});
+service.getProducts(q, jsonArray => {
+  jsonArray.forEach(product => {
+    let makechild = '';
+    makechild += `<div>`;
+    makechild += `${product.product_name}\n`;
+    makechild += `${product.product_price}`;
+    makechild += `</div>`;
+
+    tmp.innerHTML += makechild;
+  });
+});
