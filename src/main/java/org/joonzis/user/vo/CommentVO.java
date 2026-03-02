02c9bfaf61fde8b50d467d696b482caea6312897
WiHyelin <hyelinwi57@gmail.com>
2026-03-02 11:52:19 +0900

    [fix] 마이페이지 모달창문제 개선

---
+++ src/main/java/org/joonzis/user/vo/CommentVO.java
@@ -0,0 +1,12 @@
+package org.joonzis.user.vo;
+
+import java.sql.Date;
+
+public class CommentVO {
+	private int comment_id;
+	private int board_id;
+	private String member_id;
+	private String comment_content;
+	private Date created_date;
+	private Date updated_date;
+}
