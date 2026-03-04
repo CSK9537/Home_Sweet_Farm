8038cc89170b268f5c7ffc4b937cd8ad72d469e6
jinjin7576 <jinjin7576@daum.net>
2026-03-04 12:13:13 +0900

    [feat] 마이페이지 서버 기능 추가

--- src/main/java/org/joonzis/user/mapper/MypageMapper.java
+++ src/main/java/org/joonzis/user/mapper/MypageMapper.java
@@ -1,10 +1,5 @@
 package org.joonzis.user.mapper;
 
-import org.apache.ibatis.annotations.Param;
-import org.joonzis.user.dto.UserDTO;
-import org.joonzis.user.vo.UserVO;
-
 public interface MypageMapper {
 	
-    
 }
