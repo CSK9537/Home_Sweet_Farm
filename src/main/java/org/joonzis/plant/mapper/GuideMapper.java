74fe89a2c972f481ebac6cdc82a42cefc9c466b9
김아무개 <heonjeo@gmail.com>
2026-03-04 16:46:28 +0900

    [feat] 나의 식물 가이드 수정

--- src/main/java/org/joonzis/plant/mapper/GuideMapper.java
+++ src/main/java/org/joonzis/plant/mapper/GuideMapper.java
@@ -1,10 +1,11 @@
 package org.joonzis.plant.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.joonzis.plant.dto.GuideDTO;
 
 public interface GuideMapper {
 	// 가이드 정보 입력
 	public int insertGuideInfo(GuideDTO gdto);
 	// 가이드 정보 출력(plant_id)
-	public GuideDTO getGuideInfo(int plant_id);
+	public GuideDTO getGuideInfo(@Param("plant_id") int plant_id);
 }
