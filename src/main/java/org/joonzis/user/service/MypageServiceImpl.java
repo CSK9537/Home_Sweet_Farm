0933170fadaa241fac474673ace1bc1e8ec31da3
jinjin7576 <jinjin7576@daum.net>
2026-03-04 12:13:13 +0900

    [feat] 마이페이지 서버 기능 추가

--- src/main/java/org/joonzis/user/service/MypageServiceImpl.java
+++ src/main/java/org/joonzis/user/service/MypageServiceImpl.java
@@ -1,8 +1,5 @@
 package org.joonzis.user.service;
 
-
-import org.joonzis.user.mapper.MypageMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.log4j.Log4j;
@@ -10,8 +7,5 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @Service
 public class MypageServiceImpl implements MypageService{
-	
-	@Autowired
-	private MypageMapper mypagemapper;
-
+	// 2026.03.04 아직 서비스가 필요한 부분이 있을지 모르니 냅두기, 만약 이후에도 비어있게 된다면 삭제!
 }
