a229419508a9c53b396e757aef17855651b5ae28
ehdehdgkdls <ehdehdgkdls@gmail.com>
2026-03-05 09:35:51 +0900

    [fest]커뮤니티기능구현작업중

--- src/main/java/org/joonzis/qna/controller/QnaController.java
+++ src/main/java/org/joonzis/qna/controller/QnaController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.RequiredArgsConstructor;
+
 @RequestMapping("/qna")
 @Controller
 @RequiredArgsConstructor
